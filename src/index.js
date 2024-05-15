dfc0489c5d4a27417fc13acce847898c4df75e8d
Eugeshakw <eugeshakw@gmail.com>
2024-05-15 18:48:59 +0300

    start template

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter basename='/react-template'>
     <App />
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
